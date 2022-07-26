b56572c8de3048b0ed00688157812891ee11529b
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2022-07-26 19:12:43 +0900

    babel 빌드 추가

---
+++ build/routers/apiRouter.js
@@ -0,0 +1,36 @@
+import express from "express";
+import axios from "axios";
+import User from "../models/User";
+import { ensureAuthorized } from "../middlewares";
+// import admin from "../firebase";
+
+const router = express.Router();
+
+router.post("/user/signIn", async (req, res) => {
+  const response = await postFirebaseFunction(req.body);
+  const customToken = response.data;
+  console.log("커스텀 토큰 : ", customToken);
+  if (!customToken) {
+    return res.status(400).json({ errorMessage: "데이터가 올바르지 않습니다." });
+  }
+  const exists = await User.exists({ firebaseId: req.body.uid });
+  if (!exists) {
+    const createdUser = await User.create({
+      service: req.body.service,
+      email: req.body.email,
+      firebaseId: req.body.uid
+    });
+    console.log(`유저 생성 완료 ${createdUser}`);
+  }return res.status(200).json({ msg: "토큰 생성 완료", token: customToken });
+});
+
+router.get("/user/myProfile", ensureAuthorized, async (req, res) => {
+  console.log(req.user);
+  const user = await User.findOne({ firebaseId: req.user.uid });
+  console.log("몽고DB: ", user);
+  res.status(200).json(user);
+});
+
+const postFirebaseFunction = user => axios.post("https://us-central1-todaykeyword.cloudfunctions.net/createCustomToken", user).catch(error => console.log(error));
+
+export default router;
---
+++ build/server.js
@@ -0,0 +1,22 @@
+import "./db";
+import express from "express";
+import morgan from "morgan";
+import apiRouter from "./routers/apiRouter";
+
+const app = express();
+const logger = morgan("dev");
+
+app.use(logger);
+app.use(express.json());
+app.use("/assets", express.static("assets"));
+app.get("/", (req, res) => {
+  return res.send("hi");
+});
+app.get("/map", (req, res) => {
+  return res.sendFile(__dirname + "/views/naver_map.html");
+});
+app.use("/api", apiRouter);
+
+app.listen(4000, function () {
+  console.log("✅ 서버 열림 localhost:4000");
+});
