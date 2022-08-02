335a30e6f04b7ef0b9036bdaa38b348c5e116091
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2022-08-02 12:37:18 +0900

    Router의 메서드 컨트롤러로 분기

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -11,3 +11,59 @@ export const getPost = async (req, res) => {
   });
   res.json({ posts });
 };
+
+export const postCreatePost = async (req, res) => {
+  let imageUrls = [];
+  req.files.forEach((file) => imageUrls.push(file.key));
+  const post = await Post.create({
+    owner: "62dd1457e2f3f416f9a5e8df",
+    imageUrls,
+    review: req.body.review,
+  });
+  res.json({ status: "success", post });
+};
+
+export const deleteComment = async (req, res) => {
+  const { commentId } = req.params;
+  const { comment } = req.body;
+  const newComment = await Comment.findByIdAndUpdate(commentId, {
+    isDeleted: true,
+  });
+  res.json(newComment);
+};
+
+export const postEditComment = async (req, res) => {
+  const { commentId } = req.params;
+  const { comment } = req.body;
+  const newComment = await Comment.findByIdAndUpdate(commentId, {
+    comment,
+  });
+  res.json(newComment);
+};
+
+export const getComments = async (req, res) => {
+  const { postId } = req.params;
+  const comments = await Comment.find({ parentComment: null })
+    .sort({ createdAt: -1 })
+    .populate("childComments");
+  res.json(comments);
+};
+
+export const createComment = async (req, res) => {
+  const { postId } = req.params;
+  //parentComment가 null이면 null을 넣고 생성
+  //parentComment가 ObjectId면 부모 객체를 찾아서 children에 추가
+  const { parentComment, comment } = req.body;
+  const newComment = await Comment.create({
+    owner: "62dd1457e2f3f416f9a5e8df",
+    post: "62e52ace6ad637d1d2281675",
+    parentComment,
+    comment,
+  });
+  if (parentComment) {
+    await Comment.findByIdAndUpdate(parentComment, {
+      $push: { childComments: newComment },
+    });
+  }
+  res.json({ newComment });
+};
--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,81 +1,30 @@
-import Post from "../models/Post.js";
-import User from "../models/User.js";
-import Comment from "../models/Comment.js";
 import express from "express";
 import { uploadS3 } from "../s3.js";
-import multer from "multer";
 import { ensureAuthorized } from "../middlewares.js";
-import { getPost } from "../controller/postController.js";
-const upload = multer({ storage: multer.memoryStorage() });
+import {
+  getPost,
+  postCreatePost,
+  deleteComment,
+  postEditComment,
+  getComments,
+  createComment,
+} from "../controller/postController.js";
+
 const postRouter = express.Router();
 
-postRouter.get("/", ensureAuthorized, getPost);
+postRouter.get("/", ensureAuthorized, getPost); //게시글 전체 불러오기
 
-postRouter.post("/create", uploadS3.array("files"), async (req, res) => {
-  let imageUrls = [];
-  req.files.forEach((file) => imageUrls.push(file.key));
-  const post = await Post.create({
-    owner: "62dd1457e2f3f416f9a5e8df",
-    imageUrls,
-    review: req.body.review,
-  });
-  res.json({ status: "success", post });
-});
+postRouter.post("/create", uploadS3.array("files"), postCreatePost);
 
 // postRouter.get("/:id", getPost); //게시글 단 건 불러오기
 
-postRouter.delete(
-  "/:postId([0-9a-f]{24})/comment/:commentId([0-9a-f]{24})",
-  async (req, res) => {
-    const { commentId } = req.params;
-    const { comment } = req.body;
-    const newComment = await Comment.findByIdAndUpdate(commentId, {
-      isDeleted: true,
-    });
-    res.json(newComment);
-  }
-);
+postRouter
+  .route("/:postId([0-9a-f]{24})/comment/:commentId([0-9a-f]{24})")
+  .delete(deleteComment)
+  .post(postEditComment); //댓글 삭제 및 수정
 
-postRouter.post(
-  "/:postId([0-9a-f]{24})/comment/:commentId([0-9a-f]{24})",
-  async (req, res) => {
-    const { commentId } = req.params;
-    const { comment } = req.body;
-    const newComment = await Comment.findByIdAndUpdate(commentId, {
-      comment,
-    });
-    res.json(newComment);
-  }
-); //댓글 수정
+postRouter.get("/:postId([0-9a-f]{24})/comment", getComments); //댓글 전체 불러오기
 
-postRouter.get("/:postId([0-9a-f]{24})/comment", async (req, res) => {
-  const { postId } = req.params;
-  const comments = await Comment.find({ parentComment: null })
-    .sort({ createdAt: -1 })
-    .populate("childComments");
-  res.json(comments);
-}); //댓글 전체 불러오기
-
-postRouter.post("/:postId([0-9a-f]{24})/comment/create", async (req, res) => {
-  const { postId } = req.params;
-  //parentComment가 null이면 null을 넣고 생성
-  //parentComment가 ObjectId면 부모 객체를 찾아서 children에 추가
-  const { parentComment, comment } = req.body;
-  const newComment = await Comment.create({
-    owner: "62dd1457e2f3f416f9a5e8df",
-    post: "62e52ace6ad637d1d2281675",
-    parentComment,
-    comment,
-  });
-  if (parentComment) {
-    await Comment.findByIdAndUpdate(parentComment, {
-      $push: { childComments: newComment },
-    });
-  }
-  res.json({ newComment });
-}); //댓글 생성
-
-const randomImageName = (bytes = 32) =>
-  crypto.randomBytes(bytes).toString("hex");
+postRouter.post("/:postId([0-9a-f]{24})/comment/create", createComment); //댓글 생성
 
 export default postRouter;
