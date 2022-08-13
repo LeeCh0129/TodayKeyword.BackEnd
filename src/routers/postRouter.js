bccdca17c1eb551456ccaa77eb0fa2e3f88cdc2f
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2022-08-13 12:10:51 +0900

    postRouter -> router로 변경

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -10,30 +10,30 @@ import {
   postCreateComment,
 } from "../controller/postController.js";
 
-const postRouter = express.Router();
+const router = express.Router();
 
-postRouter.get("/", ensureAuthorized, getPost); //게시글 전체 불러오기
+router.get("/", ensureAuthorized, getPost); //게시글 전체 불러오기
 
-postRouter.post(
+router.post(
   "/create",
   ensureAuthorized,
   uploadS3.array("files"),
   postCreatePost
 );
 
-// postRouter.get("/:id", getPost); //게시글 단 건 불러오기
+// router.get("/:id", getPost); //게시글 단 건 불러오기
 
-postRouter
+router
   .route("/:postId([0-9a-f]{24})/comment/:commentId([0-9a-f]{24})")
   .delete(deleteComment)
   .post(postEditComment); //댓글 삭제 및 수정
 
-postRouter.get("/:postId([0-9a-f]{24})/comment", ensureAuthorized, getComments); //댓글 전체 불러오기
+router.get("/:postId([0-9a-f]{24})/comment", ensureAuthorized, getComments); //댓글 전체 불러오기
 
-postRouter.post(
+router.post(
   "/:postId([0-9a-f]{24})/comment/create",
   ensureAuthorized,
   postCreateComment
 ); //댓글 생성
 
-export default postRouter;
+export default router;
