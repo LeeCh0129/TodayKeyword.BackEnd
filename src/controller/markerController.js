538e4381d9ce1f02f5aaf4f46cf546abdab69ddf
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2022-08-13 13:17:15 +0900

    마커로 게시글 불러오기 추가

--- src/controller/markerController.js
+++ src/controller/markerController.js
@@ -1,18 +1,15 @@
 import Marker from "../models/Marker.js";
+import Post from "../models/Post.js";
 
 export const getMarker = async (req, res) => {
-  //   const newMarker = await Marker.create({
-  //     position: {
-  //       lat: 37.270952,
-  //       lng: 127.1264088,
-  //     },
-  //     store: "더진국수육국밥 강남대점",
-  //     address: "경기 용인시 기흥구 강남로 3 강남앤플러스",
-  //     category: "맛집",
-  //   });
   const marker = await Marker.find({});
-
   res.json(marker);
+};
+
+export const getPostsFromMarker = async (req, res) => {
+  const { markerId } = req.params;
+  const posts = await Post.find({ marker: markerId });
+  res.status(200).json({ posts });
 };
 
 export const postCreateMarker = (req, res) => {
