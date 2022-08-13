d977297f164bd2d512b65c349ca06e464bf7be8e
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2022-08-13 12:10:08 +0900

    PostModel 가게명을 Unique로 변경, UserModel nickName추가

--- src/models/Post.js
+++ src/models/Post.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
   {
-    store: { type: String },
+    store: { type: String, unique: true },
     category: { type: String },
     imageUrls: {
       type: [{ type: String }],
