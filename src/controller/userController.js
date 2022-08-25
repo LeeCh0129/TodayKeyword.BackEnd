4beb94148fcd523f17c4203829a82df726f9b75a
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2022-08-25 19:43:03 +0900

    로그인 유저 생성 에러 체크 제거

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,22 +2,17 @@ import axios from "axios";
 import User from "../models/User.js";
 
 export const signIn = async (req, res) => {
-  const response = await postFirebaseFunction(req.body); //인증서버 로그인 또는 회원가입 후 토큰 반환
-  const exists = await User.exists({ firebaseId: req.body.uid }); //DB에서 유저 확인
-  // const { service, email, uid, signUp: { name, nickName }} = req.body; //요청에서 회원가입에 필요한 데이터를 변수로 저장
   const { service, email, uid } = req.body; //요청에서 회원가입에 필요한 데이터를 변수로 저장
+  const response = await postFirebaseFunction(req.body); //인증서버 로그인 또는 회원가입 후 토큰 반환
+  const exists = await User.exists({ firebaseId: uid }); //DB에서 유저 확인
+  // const { service, email, uid, signUp: { name, nickName }} = req.body; //요청에서 회원가입에 필요한 데이터를 변수로 저장
   if (!exists) {
     await User.create({
       //DB에 유저 생성
       service,
       email,
       firebaseId: uid,
-    }).error((error) =>
-      res.status(400).json({
-        errorMessage:
-          "회원가입에 실패했습니다. 데이터를 확인 후 다시 요청해주세요.",
-      })
-    ); //회원가입 진행
+    });
   } else {
     return res.status(401).json({ errorMessage: "회원가입을 진행해주세요." }); //회원정보가 없으면 에러 반환
   }
